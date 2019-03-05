309095e403b3191e3a084cb8f4c0981fc04edd2e
zhangchen915 <zhangchen915@outlook.com>
2019-03-05 13:35:11 +0800

    add: drag file order

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import {DatabaseService} from '../../services/database.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-config',
@@ -8,16 +9,31 @@ import {DatabaseService} from '../../services/database.service';
   styleUrls: ['./config.component.scss']
 })
 export class ConfigComponent implements OnInit, OnDestroy {
-  todo = [];
-  done = [];
+  showList = [];
+  hideList = [];
+  formDoc: FormGroup;
+  fileInput;
 
-  constructor(private dbService: DatabaseService) {
+  @ViewChild('file') file;
+
+  constructor(private dbService: DatabaseService, private fb: FormBuilder) {
+  }
+
+  addFile() {
+    const file = this.fileInput.files[0];
+    if (!this.showList.concat(this.hideList).find(e => e.name === file.name)) {
+      this.showList.push({
+        name: file.name,
+        path: file.path,
+      });
+    }
+
+    this.file.clear();
   }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-      console.log(event);
     } else {
       transferArrayItem(event.previousContainer.data,
         event.container.data,
@@ -27,17 +43,25 @@ export class ConfigComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.formDoc = this.fb.group({
+      basicfile: [{value: undefined, disabled: false}]
+    });
+
     this.dbService.db.file.find().exec().then(res => {
       res.forEach(e => {
         const json = e.toJSON();
         console.log(e.use);
-        json.use ? this.todo[json.use - 1] = json : this.done.push(json);
+        json.use ? this.showList[json.use - 1] = json : this.hideList.push(json);
       });
     });
   }
 
   ngOnDestroy(): void {
-
+    this.showList.map((e, i) => e.use = i);
+    this.hideList.map(e => e.use = 0);
+    this.dbService.db.file.find().remove().then(() => {
+      this.showList.concat(this.hideList).forEach(e => this.dbService.db.file.insert(e));
+    });
   }
 
 }
