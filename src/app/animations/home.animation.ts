ba6ad5b2793daf1847f9418080a5aabb30e44d8d
zhangchen915 <zhangchen915@outlook.com>
2019-03-14 18:35:04 +0800

    fix: home animation

--- src/app/animations/home.animation.ts
+++ src/app/animations/home.animation.ts
@@ -2,13 +2,13 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 
 export const HomeAnimation = trigger('openClose', [
   state('0', style({
-    height: '20%',
+    height: '*',
   })),
   state('1', style({
     height: '100%',
   })),
   state('2', style({
-    height: '20%',
+    height: '*',
   })),
   transition('* => *', [
     animate('.25s')
