c5ab9003cdf409a041c0e648f76c5d1a55010d8e
zhangchen915 <zhangchen915@outlook.com>
2019-04-02 15:49:01 +0800

    remove: TabComponent

--- src/app/components/result/result.module.ts
+++ src/app/components/result/result.module.ts
@@ -3,7 +3,6 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {ResultComponent} from './result.component';
 import {LikeComponent} from './like/like.component';
-import {TabComponent} from './tab/tab.component';
 import {DefinitionComponent} from './definition/definition.component';
 import {BarRatingModule} from 'ngx-bar-rating';
 import {MaterialModule} from '../../material.module';
@@ -11,9 +10,9 @@ import {AudioDirective} from '../../directives/audio.directive';
 import {ItalicDirective} from '../../directives/italic.directive';
 
 @NgModule({
-  declarations: [ResultComponent, LikeComponent, TabComponent, DefinitionComponent, AudioDirective, ItalicDirective],
+  declarations: [ResultComponent, LikeComponent, DefinitionComponent, AudioDirective, ItalicDirective],
   imports: [BrowserModule, BarRatingModule, MaterialModule],
-  exports: [ResultComponent, LikeComponent, TabComponent]
+  exports: [ResultComponent, LikeComponent]
 })
 export class ResultModule {
 }
